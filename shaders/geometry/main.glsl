7bdc14b6fdccf4f48f4890663d1a1819026a0e35
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-10-27 15:51:56 +0100

    EDT - refactor set_out of geometry shader

--- shaders/geometry/main.glsl
+++ shaders/geometry/main.glsl
@@ -26,20 +26,18 @@ void set_out(int index)
     gs_out.normal = gs_in[index].normal;
     gs_out.tex_coords = gs_in[index].tex_coords;
     gs_out.TBN = gs_in[index].TBN;
+    gl_Position = gl_in[index].gl_Position;
 }
 
 void main()
 {
     set_out(0);
-    gl_Position = gl_in[0].gl_Position;
     EmitVertex();
 
     set_out(1);
-    gl_Position = gl_in[1].gl_Position;
     EmitVertex();
 
     set_out(2);
-    gl_Position = gl_in[2].gl_Position;
     EmitVertex();
 
     EndPrimitive();
