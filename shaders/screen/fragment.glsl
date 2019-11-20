089d4703188348ed6d6ac9e1dfef421cf4661512
Thibault Guisnet <thibault.guisnet@epita.fr>
2019-11-20 10:47:49 +0100

    EDT - add hdr (WIP, need gamma correction comprehension)

--- shaders/screen/fragment.glsl
+++ shaders/screen/fragment.glsl
@@ -20,10 +20,23 @@ uniform int rand;
 
 void main()
 {
+
+    const float gamma = 2.2;
+    const float exposure = 0.5;
+
     output_color = vec4(1);
 
     /* ------------------------------------------------------- */
     /* ------------------------------------------------------- */
 
-    output_color *= texture(texture_other0, fs_in.tex_coords);
+    //output_color *= texture(texture_other0, fs_in.tex_coords);
+
+    vec3 hdrColor = texture(texture_other0, fs_in.tex_coords).rgb;
+
+    vec3 result = hdrColor / (hdrColor + vec3(1.0));
+    //vec3 result = vec3(1.0) - exp(-hdrColor * exposure);
+
+    //result = pow(result, vec3(1.0 / gamma));
+
+    output_color *= vec4(result, 1.0);
 }
