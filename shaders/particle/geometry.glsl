b75c752fcd36e4db7ae9bcee16532fe0f04351f1
Alexandre Lamure <alexandre.lamure@epita.fr>
2020-01-28 21:56:19 +0100

    EDT - removed useless variable creation in particle geometry shader

--- shaders/particle/geometry.glsl
+++ shaders/particle/geometry.glsl
@@ -21,17 +21,14 @@ uniform vec3 offset;
 uniform float scale;
 
 
-void set_out(vec2 quad_offset, vec2 tex_coords)
+void set_out(vec2 quad_pos, vec2 tex_coords)
 {
     gs_out.tex_coords = tex_coords;
-
-    vec3 pos = gs_in[0].pos;
-    pos.xy += quad_offset;
 
     // always face the camera
     vec3 camera_right = vec3(view[0][0], view[1][0], view[2][0]);
     vec3 camera_up = vec3(view[0][1], view[1][1], view[2][1]);
-    gl_Position = projection * view * vec4((offset + (camera_right * pos.x * scale) + (camera_up * pos.y * scale)), 1);
+    gl_Position = projection * view * vec4((offset + (camera_right * quad_pos.x * scale) + (camera_up * quad_pos.y * scale)), 1);
 }
 
 void main()
